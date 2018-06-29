8ab48974e9ccc987d25cf19d1446684207428d41
Braunger, David <David.Braunger@startribune.com>
2018-06-29 16:10:39 -0500

    push changes

---
+++ sources/assets/ad.js
@@ -0,0 +1,36 @@
+<!-- Begin Outdoor Weekend Series Header Tag -->
+<script>
+  var gptadslots = [];
+  var googletag = googletag || {cmd:[]};
+</script>
+<script>
+  googletag.cmd.push(function() {
+   
+    gptadslots.push(googletag.defineSlot('/7932/website/web_sports/web_sports_outdoors/web_sports_outdoors_weekend_series', [[300,250]], 'div-gpt-ad-4205362-1')
+                             .setTargeting('pos', ['1'])
+                             .addService(googletag.pubads()));
+   
+    gptadslots.push(googletag.defineSlot('/7932/website/web_sports/web_sports_outdoors/web_sports_outdoors_weekend_series', [[300,250]], 'div-gpt-ad-4205362-2')
+                             .setTargeting('pos', ['2'])
+                             .addService(googletag.pubads()));
+    
+    gptadslots.push(googletag.defineSlot('/7932/website/web_sports/web_sports_outdoors/web_sports_outdoors_weekend_series', [[300,250]], 'div-gpt-ad-4205362-3')
+                             .setTargeting('pos', ['3'])
+                             .addService(googletag.pubads()));
+   
+    gptadslots.push(googletag.defineSlot('/7932/website/web_sports/web_sports_outdoors/web_sports_outdoors_weekend_series', [[300,250]], 'div-gpt-ad-4205362-4')
+                             .setTargeting('pos', ['4'])
+                             .addService(googletag.pubads()));
+    
+    gptadslots.push(googletag.defineSlot('/7932/website/web_sports/web_sports_outdoors/web_sports_outdoors_weekend_series', [[300,250]], 'div-gpt-ad-4205362-5')
+                             .setTargeting('pos', ['5'])
+                             .addService(googletag.pubads()));
+    
+    gptadslots.push(googletag.defineSlot('/7932/website/web_sports/web_sports_outdoors/web_sports_outdoors_weekend_series', [[300,250]], 'div-gpt-ad-4205362-6')
+                             .setTargeting('pos', ['6'])
+                             .addService(googletag.pubads()));
+
+    googletag.enableServices();
+  });
+</script>
+<!-- End Header Tag -->
